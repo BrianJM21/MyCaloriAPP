812601bf3f829aa3a0ba765655c78e3078f1a707
BrianJM21 <brianjmoedano@gmail.com>
2023-01-24 18:02:24 -0600

    User Service update

--- MyCaloriAPP/MyCaloriAPP/Entities/UserService.swift
+++ MyCaloriAPP/MyCaloriAPP/Entities/UserService.swift
@@ -6,9 +6,43 @@
 //
 
 import Foundation
-
+import Combine
+import FirebaseAuth
+import CoreData
 
 class UserService {
+    
+    // Propiedades del UserService
+    var activeUser: UserEntity?
+    
+    // Publicadores del UserService
+    
+    
+    // Funcionalidades del UserService
+    
+    // En el proceso de registro, verifica si el correo se encuentra registrado
+    func verifyEmailAlredySignedUp(_ email: String) {
+        
+        Auth.auth().signIn(withEmail: email, password: "_") {
+            
+            _, error in
+            
+            if let x = error {
+                
+                let err = x as NSError
+                
+                switch err.code {
+                    
+                    // Email ya existe en la FireBase
+                case AuthErrorCode.wrongPassword.rawValue: print("wrong password")
+                    
+                    // Email no exitse en FireBase
+                default: print("unknown error: \(err.localizedDescription)")
+
+                }
+            }
+        }
+    }
     
     
 }
